5a66190cc3b9738820ba2d02197747ed34036db1
AleenaArif30 <aleena.arif2024@gmail.com>
2024-07-29 21:04:42 +0500

    topics of events is covered

---
+++ app.js
@@ -0,0 +1,8 @@
+function zoomIn(e){
+//  e.src= "https://online.hbs.edu/Style%20Library/api/resize.aspx?imgpath=/PublishingImages/overhead-view-of-business-strategy-meeting.jpg&w=1200&h=630"
+ e.style.transform = "scale(2)"
+}
+function zoomOut(e){
+// e.src= "https://i.pinimg.com/originals/f2/a5/0d/f2a50df4f11e22db732b96692906dfe8.gif"
+e.style.transform = "scale(1)"
+}
