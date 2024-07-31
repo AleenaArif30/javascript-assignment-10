0b58b02663dac151918f0ede50fc3a527aff9e72
AleenaArif30 <aleena.arif2024@gmail.com>
2024-07-31 18:55:16 +0500

    events is done

--- app.js
+++ app.js
@@ -6,3 +6,24 @@ function zoomOut(e){
 // e.src= "https://i.pinimg.com/originals/f2/a5/0d/f2a50df4f11e22db732b96692906dfe8.gif"
 e.style.transform = "scale(1)"
 }
+
+// function myFunction(btn){
+//    var table=  document.getElementById("mytable")
+//     var row = btn.parentNode.parentNode;
+//     table.myFunction.remove()
+// }
+function myFunction(button) {
+    var row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+
+var counter = 0
+function increase(){
+    counter++
+    document.getElementById("counter").innerHTML = counter
+}
+
+function decrease(){
+    counter--
+    document.getElementById("counter").innerHTML = counter
+}
